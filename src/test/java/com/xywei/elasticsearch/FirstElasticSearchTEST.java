2253bd1522682ba03fa024a7c2e639717ab80ff4
winfuture <insoul_net@qq.com>
2019-10-27 01:40:40 +0800

    完成添加数据，查询数据步骤

--- src/test/java/com/xywei/elasticsearch/FirstElasticSearchTEST.java
+++ src/test/java/com/xywei/elasticsearch/FirstElasticSearchTEST.java
@@ -1,9 +1,15 @@
 package com.xywei.elasticsearch;
 
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.xcontent.XContent;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.After;
 import org.junit.Before;
@@ -45,6 +51,7 @@ public class FirstElasticSearchTEST {
     public void closeClient() {
         if (null != client) {
             client.close();
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>client closed>>>>>>>>>>>>>>>>>>>>");
         }
     }
 
@@ -52,4 +59,98 @@ public class FirstElasticSearchTEST {
     public void testClient() {
         System.out.println("client:>>>>>>>>>>>>>>>" + client);
     }
+
+    /**
+     * @Description 测试添加document
+     * @Author future
+     * @DateTime 2019/10/27 0:24
+     **/
+    @Test
+    public void testInsert() throws Exception {
+
+        //准备数据
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
+                .startObject()
+                .field("name", "D")
+                .field("age", "40")
+                .field("dept", "Java")
+                .startObject("address")
+                .field("country", "US")
+                .field("province", "NY")
+                .field("city", "HY")
+                .endObject()
+                .endObject();
+
+        IndexResponse response = client.prepareIndex("company", "employee", "004")
+                .setSource(xContentBuilder).get();
+
+        System.out.println("insert>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + response.getVersion());
+    }
+
+    /**
+     * @Description 批量插入document 方法1，TODO 是否还有其他方法？
+     * @Author future
+     * @DateTime 2019/10/27 0:59
+     **/
+    @Test
+    public void testInsertBatch() throws Exception {
+
+        client.prepareIndex("school", "Python", "001")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "A").field("age", "18").field("gender", "W").endObject()
+                ).get();
+
+        client.prepareIndex("school", "Python", "002")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "B").field("age", "25").field("gender", "M").endObject()
+                ).get();
+
+        client.prepareIndex("school", "Python", "003")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "C").field("age", "30").field("gender", "M").endObject()
+                ).get();
+
+        client.prepareIndex("school", "Python", "004")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "D").field("age", "40").field("gender", "W").endObject()
+                ).get();
+
+        client.prepareIndex("school", "Python", "005")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "E").field("age", "40").field("gender", "W").endObject()
+                ).get();
+
+        client.prepareIndex("school", "Python", "006")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "F").field("age", "30").field("gender", "W").endObject()
+                ).get();
+
+        client.prepareIndex("school", "Python", "007")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject().field("name", "G").field("age", "30").field("gender", "M").endObject()
+                ).get();
+
+
+    }
+
+    /**
+     * @Description 查询数据
+     * @Author future
+     * @DateTime 2019/10/27 1:09
+     **/
+    @Test
+    public void testGet() {
+
+        //查询单条
+        GetResponse response = client.prepareGet("school", "Java", "003").get();
+        System.out.println(">>>>>>>>>>>>>>>" + response);
+
+        //多条
+        SearchResponse school = client.prepareSearch("school")
+                .setTypes("Python")
+                .get();
+        System.out.println(">>>>>>>>>>>>>>>" + school);
+    }
+
+
 }
